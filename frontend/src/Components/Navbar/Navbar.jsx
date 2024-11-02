f93d234ffa009b366adf73b91fa33ef34f2c795a
carconex <karmeconemar@gmail.com>
2024-11-02 10:03:24 +0100

    resalto de elementos del menu

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/carrito.png'
 
 export const Navbar = () => {
+  const [menu,setMenu] = useState("inici");
   return (
     <div className='navbar'>
       <div className="nav-logo">
@@ -12,10 +13,13 @@ export const Navbar = () => {
         <p className='baixfoto'>LA TRAVESSA</p>
       </div>
       <ul className="nav-menu">
-        <li>INICI <hr/></li>
-        <li>MENÚ</li>
-        <li>GALERIA</li>
-        <li>NOSALTRES</li>
+        {/* cada element del menu te un event que seteja el hookal valor indicat */}
+        {/* si dona la casualitat que te el valor del hookmenu concideix en el elemnt */}
+        {/* li afegim un element <hr/> que el que fa es crear una linia per a destacar */}
+        <li onClick={() => {setMenu("inici")}}>INICI {menu==="inici" ? <hr/> : <></>}</li>
+        <li onClick={() => {setMenu("menu")}}>MENÚ {menu==="menu" ? <hr/> : <></>}</li>
+        <li onClick={() => {setMenu("galeria")}}>GALERIA {menu==="galeria" ? <hr/> : <></>}</li>
+        <li onClick={() => {setMenu("nosaltres")}}>NOSALTRES{menu==="nosaltres" ? <hr/> : <></>}</li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>

5cc1b4184240fd4bc9c14c8a54a4595bbea26dfe
carconex <karmeconemar@gmail.com>
2024-11-13 10:48:57 +0100

    cambio en el loggin

--- frontend/src/Pages/LoginSignUp.jsx
+++ frontend/src/Pages/LoginSignUp.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
 import './CSS/LoginSignUp.css';
+import { Link } from 'react-router-dom';
 
 const LoginSignUp = () => {
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        <h1>Registra't</h1>
+        <h1>Inicia sessió</h1>
+        <p className="loginsignup-login">Ja tens un compte? <span><Link className='link-text' to='/'>Registra't</Link></span></p>
         <div className="loginsignup-fields">
-          <input type="text" placeholder='Nom'/>
+          {/* <input type="text" placeholder='Nom'/> */}
           <input type="email" placeholder='Adreça electrònica'/>
           <input type="password" placeholder='Contrasenya'/>
         </div>
-        <button>Continua</button>
-        <p className="loginsignup-login">Ja tens un compte? <span>Inicia sessió</span></p>
-        <div className="loginsignup-agree">
-          <input type="checkbox" />
-          <p>Accepte els termes d'us i privacitat</p>
-        </div>
+        <button>LogIn</button>
+        {/* s'ha de linkejar el inici de sessió en altra pesatanya */}
       </div>
     </div>
   )
